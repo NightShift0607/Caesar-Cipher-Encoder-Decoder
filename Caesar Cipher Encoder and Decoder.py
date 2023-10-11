1e942db7746119a7ad97f75ffcbf76518f2007b5
Rajat Gupta <142142387+NightShift0607@users.noreply.github.com>
2023-10-11 16:02:23 +0530

    Add files via upload

---
+++ Caesar Cipher Encoder and Decoder.py
@@ -0,0 +1,46 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(message, change, rule):
+    # Caesar cipher.
+    req_message = ""
+    for letter in message:
+        if letter not in alphabet:
+            req_message += letter
+        elif rule == "encode":
+            j = alphabet.index(letter)
+            req_message += alphabet[j+change]
+        elif rule == "decode":
+            j = alphabet.index(letter)
+            req_message += alphabet[j-change]
+    print(f"The {rule}d message is {req_message}")
+
+print("""
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88          """)
+print("""
+           88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88                                             
+""")
+
+while True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 25   # if the user enters the shift more than 25 
+    caesar(text, shift, direction)
+    response = input("Do you want to restart Cipher Program? Yes or No:\n").upper()
+    if response == "NO":
+        print("Goodbye")
+        break
